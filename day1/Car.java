df3408df3c102e39d1dcc68c3e1dd9591536fdc2
sheenavr2018 <42091563+sheenavr2018@users.noreply.github.com>
2022-11-27 22:14:04 +0530

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Car {
+	int wheels;
+	String name="Punto";
+	public void driveCar()
+	{
+		System.out.println("Drive a car");
+		
+	}
+
+	public static void main(String[] args)
+	{
+		Car myCar=new Car();
+		myCar.driveCar();
+		
+	}
+	private void applyBrake() {
+		
+
+	}
+
+}
